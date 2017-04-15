14e0cd29506c2acf26a94f9e8324f708d4871cc8
Your Name <you@example.com>
2017-04-15 14:54:04 +0400

    pong

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var countUp = function(countTo){
+ var result = [];
+ for(var i = 1; i <= countTo; i++){
+     var index = result.indexOf(i);
+     if(i % 15 === 0) {
+       result.splice(index, 0);
+       result.push('pingpong');
+     }else if(i % 5 === 0) {
+       result.splice(index, 0);
+       result.push('pong');
+     }else if(i % 3 === 0) {
+       result.splice(index, 0);
+       result.push('ping');
+     }else{
+       result.push(i);
+     }
+ }  return result;
+};
+
+
+$(document).ready(function(){
+ $("#pingpong").submit(function(event){
+   $("#output").empty();
+   var countTo = parseInt($("input#countTo").val());
+   var output = countUp(countTo);
+
+     output.forEach(function(item){
+     $("#output").append('<li>' + item + '</li>');
+   });
+   event.preventDefault();
+ });
+});
